f9a389dc5c103414bb1010f7261c6e02ab065c45
Tomasz Ziolkowski <tomasz.ziolkowski@allegrogroup.com>
2016-06-10 08:00:45 +0200

    Correct license + required python 2.7.x or 3.4.x

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ import os
 import sys
 from distutils.core import setup
 
-from pip.req import parse_requirements
 from setuptools import find_packages
 
-assert sys.version_info >= (3, 4), "Python 3.4+ required."
+assert sys.version_info == (2, 7) or sys.version_info == (3, 4), "Python 2.7.x or 3.4.x is required."
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(current_dir, 'README.rst')) as readme_file:
@@ -27,7 +26,7 @@ setup(
     install_requires=[],
     keywords='varnish,varnishlog,varnishstat',
     platforms=['any'],
-    license='Apache Software License v2.0',
+    license='FreeBSD',
     packages=find_packages('src'),
     include_package_data=True,
     package_dir={'': 'src'},
